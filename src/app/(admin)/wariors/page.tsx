4eb61e8723d9c737573b2a643accb15e40f37c69
Dimitry Minchukov <jerek.d.lem@gmail.com>
2025-04-19 01:10:04 +0300

    fix wariors page

--- src/app/(admin)/wariors/page.tsx
+++ src/app/(admin)/wariors/page.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
 
-interface pageProps {
-  children: React.ReactNode;
-}
-
-const Page = ({ children }: pageProps) => {
+const Page = () => {
   return (
     <div>
       <h1 className="text-green-600">Wariors</h1>
-      {children}
     </div>
   );
 };
